ac81db7fdfbc731fb3d125ad0e6aa79bd382f84a
ahmedmab <ahmedmab87@gmail.com>
2020-11-21 22:31:17 +0100

    table 2

--- src/app/vma-trainer/vma-trainer.component.ts
+++ src/app/vma-trainer/vma-trainer.component.ts
@@ -6,14 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vma-trainer.component.css']
 })
 export class VmaTrainerComponent implements OnInit {
-vma:number
-vma80 
+vma
+vma60
+vma65
+vma70
+vma75
+vma80
 vma85
-vma90 
+vma90
+vma95
 vma100
 vma105 
 vma110 
 vma115
+// allur de 100 m
+vma_ms 
+vma_30s:number
+vma_100 
+second
+minut
+metre 
 
   constructor() { }
 
@@ -21,13 +33,25 @@ vma115
   }
 
   calcul(){
-this.vma80 = (this.vma*80)/100
-this.vma85 = (this.vma*85)/100
-this.vma90 = (this.vma*90)/100
-this.vma100 = this.vma
-this.vma105 = (this.vma*105)/100
-this.vma110 = (this.vma*110)/100
-this.vma115 = (this.vma*115)/100
+this.second = ' s'   
+this.minut = ' min'
+this.metre = ' m'
+this.vma_ms = this.vma * 1000 / 3600
+this.vma_30s = this.vma_ms
+this.vma_100 = 100 / this.vma_ms
+this.vma60 = (this.vma*60)/100 + " km/h"
+this.vma65 = (this.vma*65)/100 + " km/h"
+this.vma70 = (this.vma*70)/100 + " km/h"
+this.vma75 = (this.vma*75)/100 + " km/h"
+this.vma80 = (this.vma*80)/100 + " km/h"
+this.vma85 = (this.vma*85)/100 + " km/h"
+this.vma90 = (this.vma*90)/100 + " km/h"
+this.vma95 = (this.vma*95)/100 + " km/h"
+this.vma100 = this.vma + " km/h"
+this.vma105 = (this.vma*105)/100 + " km/h"
+this.vma110 = (this.vma*110)/100 + " km/h"
+this.vma115 = (this.vma*115)/100 + " km/h"
+ console.warn(this.vma_30s)
   }
 
 }
