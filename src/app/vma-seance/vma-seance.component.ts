ee32719965ae2a9cac40a75861e63ed72deea349
ahmedmab <ahmedmab87@gmail.com>
2020-12-20 14:44:33 +0100

    formbuilder

--- src/app/vma-seance/vma-seance.component.ts
+++ src/app/vma-seance/vma-seance.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormBuilder, Validators } from '@angular/forms'
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 
@@ -11,60 +11,63 @@ import 'jspdf-autotable';
   styleUrls: ['./vma-seance.component.css']
 })
 export class VmaSeanceComponent implements OnInit {
-  seriForm = new FormGroup({
-    percent: new FormControl(''),
-    vmaVal: new FormControl(''),
-    param: new FormControl(''),
-    rep: new FormControl(''),
+  seriForm = this.fb.group({
+    percent: ['', Validators.required],
+    vmaVal: ['', Validators.required],
+    param: ['', Validators.required],
+    rep: ['', Validators.required],
   })
 
-avec:boolean = true
-inputype:string = ' seconds'
-num:number=1
-recuperation
-min:number
-sec:number
-seriGrp:any[]=[]
-cardType:string
- mOrs:string
- volumeSeri:number
- volumeTotale:number=0
- echauf:string = 'course de 20 min. à 65% de la VMA ; Étirements '
+  avec: boolean = true
+  inputype: string = ' seconds'
+  num: number = 1
+  min: number
+  sec: number
+  seriGrp: any[] = []
+  cardType: string
+  mOrs: string
+  volumeSeri: number
+  volumeTotale: number = 0
+  echauf: string = 'course de 20 min. à 65% de la VMA ; Étirements '
 
-  constructor() { }
+  constructor(private fb:FormBuilder ) { }
 
   ngOnInit(): void {
 
   }
 
- 
-  setRec(efort:number, type:string){
+  //fonction pour calculer la recuperation inter-repetition
+  setRec(efort: number, type: string) {
     let result;
     if (type == 'm') {
-      
-        if (efort > 100){result = 'cas de plus 100';}
-        else if (efort < 100){result = 'cas de moins 100';}
-        else {result = 'egal a 100';}
-          
-      }
-    
+
+      if (efort > 100) { result = 'cas de plus 100'; }
+      else if (efort < 100) { result = 'cas de moins 100'; }
+      else { result = 'egal a 100'; }
+
+    }
+
     else {
       result = 'faut calculer rec avec le temps';
     }
     return result
   }
 
-  togle(){
+  togle() {
     this.avec = !this.avec
     this.avec ? this.inputype = ' seconds' : this.inputype = ' métres'
   }
-  
-  submit(f){
-    if(this.inputype == ' métres' && f.param>0) {f.mOrs = 'm' 
-    f.volumeSeri = parseInt(f.param, 10) * parseInt(f.rep, 10)}  
-    else if (this.inputype == ' seconds' && f.param>0) {f.mOrs = 's'
-    f.volumeSeri = ((parseInt(f.param, 10) * (parseInt(f.vmaVal, 10) * parseInt(f.percent, 10) / 100) / 3.6) * parseInt(f.rep, 10)).toFixed(2)} 
-    else {this.cardType = ''}
+
+  submit(f) {
+    if (this.inputype == ' métres' && f.param > 0) {
+      f.mOrs = 'm'
+      f.volumeSeri = parseInt(f.param, 10) * parseInt(f.rep, 10)
+    }
+    else if (this.inputype == ' seconds' && f.param > 0) {
+      f.mOrs = 's'
+      f.volumeSeri = ((parseInt(f.param, 10) * (parseInt(f.vmaVal, 10) * parseInt(f.percent, 10) / 100) / 3.6) * parseInt(f.rep, 10)).toFixed(2)
+    }
+    else { this.cardType = '' }
 
     this.seriGrp.push(f)
     this.num++
@@ -77,98 +80,99 @@ cardType:string
     })
     console.warn(f)
   }
-  renitialiser(){
+  renitialiser() {
     this.seriGrp = []
     this.num = 1
     this.volumeTotale = 0
   }
-  delet(index:number){
+  delet(index: number) {
     this.volumeTotale = this.volumeTotale - parseInt(this.seriGrp[index].volumeSeri, 10)
-    this.seriGrp.splice(index,1)
-    this.num --
+    this.seriGrp.splice(index, 1)
+    this.num--
   }
   //convertir les seconds en min et sec
   MyTime(STemps) {
     var result = "";
     // suppressions des chiffres après la virgule
-    STemps = (Math.round(STemps * 10) )/ 10;
-  
-    var MyMinut = (STemps - ( STemps % 60) ) / 60;
-    if (MyMinut > 0) {result = result + MyMinut + " mn " };
-    var MySecond = (Math.round((STemps % 60)*10)) / 10 ;
-    if (MySecond > 0) {result = result + MySecond + " s" };
-    return result }
+    STemps = (Math.round(STemps * 10)) / 10;
 
-     // data pour le document PDF
+    var MyMinut = (STemps - (STemps % 60)) / 60;
+    if (MyMinut > 0) { result = result + MyMinut + " mn " };
+    var MySecond = (Math.round((STemps % 60) * 10)) / 10;
+    if (MySecond > 0) { result = result + MySecond + " s" };
+    return result
+  }
+
+  // data pour le document PDF
 
   title = `Seance d'entrainment VMA`;
 
-  convertoPdf(){
-    let j=1;
-    this.seriGrp.forEach(el=>{
+  convertoPdf() {
+    let j = 1;
+    this.seriGrp.forEach(el => {
       console.log(el)
 
     })
     var doc = new jsPDF('portrait', 'px', 'a4');
 
     doc.setFontSize(20);
-    doc.text(`Seance d'entrainment VMA`, doc.internal.pageSize.getWidth()/2, 20, {align:"center"});
+    doc.text(`Seance d'entrainment VMA`, doc.internal.pageSize.getWidth() / 2, 20, { align: "center" });
 
     doc.setFontSize(15);
     doc.setTextColor(20);
     doc.text(`Echauffement : ${this.echauf}`, 30, 60);
-   
+
     doc.setFontSize(11);
     doc.setTextColor(100);
 
-    this.seriGrp.forEach(element =>{
-    
-     
+    this.seriGrp.forEach(element => {
+
+
       (doc as any).autoTable({
-        margin: {top: 50},
-               
-        theme : 'striped',
+        margin: { top: 50 },
+
+        theme: 'striped',
         styles: {
           fontSize: 10,
           font: 'helvetica',
           cellPadding: 2,
           minCellHeight: 2,
-      },
-      
-      headStyles: {
-        fillColor: [255, 255, 255],
-        textColor: [0, 0, 10],
-        fontSize: 18,
-        fontStyle: 'italic',
-        padding: 0,
-    },
-    columnStyles: {
-      0: {fontStyle: 'bold'},
-      1: {halign: 'center'},
-    },
-        head: [['serie '+ j++, '']],
-  
-        body: [
-               [{ content: `${element.percent} % ${element.vmaVal} Km/h   ( ${element.rep} X ${element.param} ${element.mOrs} )` , colSpan: 2, styles: { halign: 'center',fontSize: 13, fontStyle: 'bold'} }],
-               ['Vitesse :', element.vmaVal*element.percent/100 + " Km/h"],
-               ['Volume de Travail :', element.rep*element.param],
-               ['Récuperation :', this.MyTime(element.param*2/3)]
-              ],
-  
-         });
-      })
+        },
 
-      doc.setFontSize(15);
-      doc.setTextColor(20);
+        headStyles: {
+          fillColor: [255, 255, 255],
+          textColor: [0, 0, 10],
+          fontSize: 18,
+          fontStyle: 'italic',
+          padding: 0,
+        },
+        columnStyles: {
+          0: { fontStyle: 'bold' },
+          1: { halign: 'center' },
+        },
+        head: [['serie ' + j++, '']],
+
+        body: [
+          [{ content: `${element.percent} % ${element.vmaVal} Km/h   ( ${element.rep} X ${element.param} ${element.mOrs} )`, colSpan: 2, styles: { halign: 'center', fontSize: 13, fontStyle: 'bold' } }],
+          ['Vitesse :', element.vmaVal * element.percent / 100 + " Km/h"],
+          ['Volume de Travail :', element.rep * element.param],
+          ['Récuperation :', this.MyTime(element.param * 2 / 3)]
+        ],
+
+      });
+    })
+
+    doc.setFontSize(15);
+    doc.setTextColor(20);
 
     let finalY = (doc as any).lastAutoTable.finalY + 20; // The y position on the page
-    doc.text(`Volume totale : ${this.volumeTotale} m`, doc.internal.pageSize.getWidth()/2, finalY, {align:"center"})
+    doc.text(`Volume totale : ${this.volumeTotale} m`, doc.internal.pageSize.getWidth() / 2, finalY, { align: "center" })
     // Open PDF document in new tab
     doc.output('dataurlnewwindow')
     // Download PDF document  
     doc.save('séance-VMA.pdf');
-    }
+  }
 
-   
-  
+
+
 }
--- src/app/vma-trainer/vma-trainer.component.ts
+++ src/app/vma-trainer/vma-trainer.component.ts
@@ -6,73 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vma-trainer.component.css']
 })
 export class VmaTrainerComponent implements OnInit {
-vma
-vma60
-vma65
-vma70
-vma75
-vma80
-vma85
-vma90
-vma95
-vma100
-vma105 
-vma110 
-vma115
-// allur de 100 m
-vma_ms 
-vma_30s:number
-vma_100 
-second
-minut
-metre 
+  vma
+  vma60
+  vma65
+  vma70
+  vma75
+  vma80
+  vma85
+  vma90
+  vma95
+  vma100
+  vma105
+  vma110
+  vma115
+  // allur de 100 m
+  vma_ms
+  vma_30s: number
+  vma_100
+  second
+  minut
+  metre
 
-tab1:boolean= true
+  tab1: boolean = true
   constructor() { }
 
   ngOnInit(): void {
   }
-  tabone(){
-    this.tab1= true
- 
-  }
-  tabtow(){
-    this.tab1= false
-  }
-  
+  tabone() {
+    this.tab1 = true
 
-  calcul(){
-   
-this.second = ' s'   
-this.minut = ' min'
-this.metre = ' m'
-this.vma_ms = this.vma * 1000 / 3600
-this.vma_30s = this.vma_ms
-this.vma_100 = 100 / this.vma_ms
-this.vma60 = (this.vma*60)/100 + " km/h"
-this.vma65 = (this.vma*65)/100 + " km/h"
-this.vma70 = (this.vma*70)/100 + " km/h"
-this.vma75 = (this.vma*75)/100 + " km/h"
-this.vma80 = (this.vma*80)/100 + " km/h"
-this.vma85 = (this.vma*85)/100 + " km/h"
-this.vma90 = (this.vma*90)/100 + " km/h"
-this.vma95 = (this.vma*95)/100 + " km/h"
-this.vma100 = this.vma + " km/h"
-this.vma105 = (this.vma*105)/100 + " km/h"
-this.vma110 = (this.vma*110)/100 + " km/h"
-this.vma115 = (this.vma*115)/100 + " km/h"
+  }
+  tabtow() {
+    this.tab1 = false
   }
 
-   MyTime(STemps) {
+
+  calcul() {
+
+    this.second = ' s'
+    this.minut = ' min'
+    this.metre = ' m'
+    this.vma_ms = this.vma * 1000 / 3600
+    this.vma_30s = this.vma_ms
+    this.vma_100 = 100 / this.vma_ms
+    this.vma60 = (this.vma * 60) / 100 + " km/h"
+    this.vma65 = (this.vma * 65) / 100 + " km/h"
+    this.vma70 = (this.vma * 70) / 100 + " km/h"
+    this.vma75 = (this.vma * 75) / 100 + " km/h"
+    this.vma80 = (this.vma * 80) / 100 + " km/h"
+    this.vma85 = (this.vma * 85) / 100 + " km/h"
+    this.vma90 = (this.vma * 90) / 100 + " km/h"
+    this.vma95 = (this.vma * 95) / 100 + " km/h"
+    this.vma100 = this.vma + " km/h"
+    this.vma105 = (this.vma * 105) / 100 + " km/h"
+    this.vma110 = (this.vma * 110) / 100 + " km/h"
+    this.vma115 = (this.vma * 115) / 100 + " km/h"
+  }
+
+  MyTime(STemps) {
     var result = "";
     // suppressions des chiffres après la virgule
-    STemps = (Math.round(STemps * 10) )/ 10;
-  
-    var MyMinut = (STemps - ( STemps % 60) ) / 60;
-    if (MyMinut > 0) {result = result + MyMinut + " mn " };
-    var MySecond = (Math.round((STemps % 60)*10)) / 10 ;
-    if (MySecond > 0) {result = result + MySecond + " s" };
-    return result }
-  
+    STemps = (Math.round(STemps * 10)) / 10;
+
+    var MyMinut = (STemps - (STemps % 60)) / 60;
+    if (MyMinut > 0) { result = result + MyMinut + " mn " };
+    var MySecond = (Math.round((STemps % 60) * 10)) / 10;
+    if (MySecond > 0) { result = result + MySecond + " s" };
+    return result
+  }
+
 
 }
