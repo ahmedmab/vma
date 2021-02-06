e5aad09c681069b7fde814e1d36b584925d3ec81
ahmedmab <ahmedmab87@gmail.com>
2021-02-06 14:44:31 +0100

    contact done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import '../assets/smtp.js';
+declare let Email: any;
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vma';
+  title = 'vma up';
+
+  constructor(
+  ) { }
+  
+  ngOnInit() {
+  }
+  msgForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    msg: new FormControl('', Validators.required)
+  })
+
+  onSubmit(f) {
+  
+  Email.send({
+  Host : "smtp.gmail.com",
+  Username : "vmaupcontact@gmail.com",
+  Password : "plsbyuntuzigzocg",
+  To : `vmaupcontact@gmail.com`,
+  From : `vmaupcontact@gmail.com`,
+  Subject : `message de ${f.name}`,
+  Body : `
+  <br/> <b>Name: </b>${f.name} <br /> <b>Email: </b>${f.email}<br /><br> <b><b>Message:</b> <br /> ${f.msg}<br>`
+  }).then( message => {alert(message); f.resetForm(); } );
+
+  f.reset();
+  
+  }
  
 }
