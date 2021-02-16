2a84296b59274e7e31f9e5e5ee7eabcce2d81e5a
ahmedmab <ahmedmab87@gmail.com>
2021-02-17 00:24:21 +0100

    updat function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,4 @@
 import * as functions from 'firebase-functions';
-//const universal = require(`${process.cwd()}/dist/vma/server/main.js`).app;
-//export const ssr = functions.https.onRequest(universal);
-import * as path from 'path';
+const universal = require(`${process.cwd()}/dist/vma/server/main.js`).app;
+export const ssr = functions.https.onRequest(universal);
 
-
-const universal = require(path.resolve(__dirname, '../dist/vma/server/main')).app;
-
-export const ssr = functions.runWith({ memory: "2GB", timeoutSeconds: 120 }).https.onRequest(universal());
