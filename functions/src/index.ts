4396c80e8abb78be6943da2f11bc145eb6dd0008
ahmedmab <ahmedmab87@gmail.com>
2021-02-17 02:17:50 +0100

    updat function timout 300

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,10 @@
 import * as functions from 'firebase-functions';
 const universal = require(`${process.cwd()}/dist/vma/server/main.js`).app;
-export const ssr = functions.https.onRequest(universal);
+//export const ssr = functions.https.onRequest(universal);
 
+  export const ssr  = functions
+    .runWith({
+        timeoutSeconds: 300,
+        memory: '1GB'
+    })
+    .https.onRequest(universal);
