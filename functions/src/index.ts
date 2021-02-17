c314efcac488cc74396c3ff636a6cba1693ab2ae
ahmedmab <ahmedmab87@gmail.com>
2021-02-17 19:27:16 +0100

    netlify

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,3 @@
 import * as functions from 'firebase-functions';
 const universal = require(`${process.cwd()}/dist/vma/server/main.js`).app;
-//export const ssr = functions.https.onRequest(universal);
-
-  export const ssr  = functions
-    .runWith({
-        timeoutSeconds: 300,
-        memory: '1GB'
-    })
-    .https.onRequest(universal);
+  export const ssr  = functions.runWith({timeoutSeconds: 400, memory: '2GB'}).https.onRequest(universal);
--- server.ts
+++ server.ts
@@ -40,7 +40,7 @@ export function app(): express.Express {
 // function run(): void {
 //   const port = process.env.PORT || 4000;
 
-  // Start up the Node server
+//   Start up the Node server
 //   const server = app();
 //   server.listen(port, () => {
 //     console.log(`Node Express server listening on http://localhost:${port}`);
